683aaabe616e1781405e84a2e14e8e72ae5a4e05
Harsh <harshsheetal18@gmail.com>
2017-09-15 18:39:24 -0400

    Adding new Project

---
+++ conf.js
@@ -0,0 +1,11 @@
+exports.config ={
+
+  directConnection:true,
+  capabilities:{
+    'browserName': 'chrome'
+  },
+
+
+
+  specs:['spec_testcase_one.js']
+};
---
+++ spec_testcase_one.js
@@ -0,0 +1,9 @@
+describe('Hotel Home Page', function(){
+it('First_test_case', function(){
+
+  browser.get('https://www.priceline.com');
+
+  browser.quit();
+});
+
+});
