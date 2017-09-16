75a1b7e8c986c2b0c5c45b0d68e48f9c56d5899a
Harsh <harshsheetal18@gmailcom>
2017-09-16 13:02:24 -0400

    added test cases

--- spec_testcase_one.js
+++ spec_testcase_one.js
@@ -1,9 +1,18 @@
 describe('Hotel Home Page', function(){
 it('First_test_case', function(){
+  browser.get('http://www.way2automation.com/angularjs-protractor/calc/');
+  element(by.model("first")).sendKeys("5");
+  element(by.model("second")).sendKeys("6");
+  element(by.id("gobutton")).click();
 
-  browser.get('https://www.priceline.com');
+element(by.binding("latest")).getText().then(function(text){
+    console.log(text);
+})
 
-//  browser.quit();
+
+
+  browser.sleep(5000);
+
 });
 
 });
