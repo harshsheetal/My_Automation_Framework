8dd2dd9714af8728a91955036906b9e076da98c8
Harsh <harshsheetal18@gmail.com>
2017-09-27 18:11:12 -0400

    New code added

--- conf.js
+++ conf.js
@@ -9,6 +9,12 @@ exports.config ={
 onPrepare:function(){
   browser.ignoreSynchronization = true;
   browser.driver.manage().window().maximize();
+
+   var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+   jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+         savePath: './e2e-reports/',
+         takeScreenshots: true,
+       }));
 },
 
 
--- spec_testcase_one.js
+++ spec_testcase_one.js
@@ -1,21 +1,35 @@
+var EC= protractor.ExpectedConditions;
+
 describe('Hotel Home Page', function(){
 
-//  "use strict"
+  "use strict"
 it('First_test_case', function(){
   browser.get('https://www.yahoo.com/');
+
+  browser.wait(EC.urlContains('yahoo'),5000);
 //element(by.id("uh-signin")).click();
 element(by.xpath("//a[@id='uh-signin']")).click();
 browser.sleep(5000);
+browser.wait(EC.urlContains('login'),5000);
 
 element(by.xpath("//input[@name='username']")).sendKeys("pinkusheetal");
 element(by.xpath("//input[@name='signin']")).click();
-browser.sleep(5000);
+browser.sleep(2000);
+browser.wait(EC.urlContains('password'),5000);
 element(by.xpath("//input[@name='password']")).sendKeys("harsh18dd");
-browser.sleep(5000);
+browser.sleep(2000);
 element(by.xpath("//button[@id='login-signin']")).click();
-browser.sleep(5000);
+browser.sleep(2000);
+
+element(by.xpath("//button[@title='Profile']")).click();
+browser.sleep(4000);
+element(by.xpath("//a[@id='uh-signout']")).click();
+browser.sleep(4000);
 })
 
   browser.sleep(5000);
+});
 
+afterAll(function(done){
+    process.nextTick(done);
 });
